c54235e536d4089898b1f722d17d43e41dd243db
elise <Elisenovaes10@gmail.com>
2024-04-07 11:43:24 -0300

    Arquivo principal

--- main.py
+++ main.py
@@ -2,30 +2,77 @@ import pygame
 import assets
 import configs
 
+from objects.bird import Bird
+from objects.floor import Floor
 from objects.bg import Bg
+from objects.pipe import Pipe
+from objects.start_message import StartMessage
+from objects.over_message import OverMessage
+from objects.score import Score
 
 pygame.init()
-
+       
 screen = pygame.display.set_mode((configs.screen_width, configs.screen_height))
 clock = pygame.time.Clock()
+pipe_create_event = pygame.USEREVENT
 running = True
+gameover = False
+gamestarted = False
+
 
 assets.load_sprites()
+assets.load_audios()
 
 sprites = pygame.sprite.LayeredUpdates()
 
-Bg(0, sprites)
-Bg(1, sprites)
+def create_sprites():
+    Bg(0, sprites)
+    Bg(1, sprites)
+    Floor(0, sprites)
+    Floor(1, sprites)
+    return Bird(sprites), StartMessage(sprites), Score(sprites)
+
+bird, start_message, score = create_sprites()
+
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pipe_create_event:
+            Pipe(sprites)
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE and not gamestarted and not gameover:
+                gamestarted = True
+                start_message.kill()
+                pygame.time.set_timer(pipe_create_event, 1500)
+            if event.key == pygame.K_ESCAPE and gameover:
+                gameover = False
+                gamestarted = False
+                sprites.empty()
+                bird, start_message, score = create_sprites()
 
-    screen.fill("purple")
+        bird.handleEvent(event)
+
+    screen.fill(0)
 
     sprites.draw(screen)
-    sprites.update()
+
+    if gamestarted and not gameover:
+        sprites.update()
+
+    if bird.checkCollision(sprites) and not gameover:
+        gameover = True 
+        gamestarted = False 
+        over_message = OverMessage(sprites)  
+        pygame.time.set_timer(pipe_create_event, 0)
+        assets.play_audio("hit")
+
+    for sprite in sprites:
+        if type(sprite) is Pipe and sprite.passedis():
+            score.value += 1
+            assets.play_audio("point")
+
 
     pygame.display.flip()
     clock.tick(configs.fps)
